6c6af28d2100b23e07c3e18d64440d3e0b2d1e87
Seb <lolbonniecraft@gmail.com>
2026-09-08 12:54:35 -0700

    Convertidor de pesos a robux

---
+++ robux_converter.py
@@ -0,0 +1,13 @@
+def convertidor_robux(pesos):
+    TASA_CAMBIO = 0.25
+    return pesos * TASA_CAMBIO
+
+try: 
+    monto_pesos = float(input("Ingresa la cantidad de pesos (MXN): "))
+
+    resultado = convertidor_robux(monto_pesos)
+    
+    print(f"Te alcanza para aproximadamente: {int(resultado)} Robux!")
+
+except ValueError:
+    print("Ingresa un valor")

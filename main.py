26f40f90bd93aa5b991ad5ef9c20c29015280a05
Dr.Nsanity <Luisalvarado00z22@gmail.com>
2026-09-08 12:27:07 -0700

    Created branches for each feature

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-print("Bienvenido al convertidor divisas")
+print("Bienvenido al convertidor divisas de MXN a otras monedas")
 input("Seleccione una de las siguientes opciones: \n1. Convertir de MXN a BOL \n2. Convertir de MXN a WON \n3. MXN a ROBUX \n4. MXN a CUP \n5. MXN a USD")
